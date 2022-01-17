357e70c56a948405962eef639607dfd01ed804e3
VasyaPikh <pikhvasya@gmail.com>
2022-01-17 17:19:05 +0000

    Add js

--- task-one/task-one.js
+++ task-one/task-one.js
@@ -1,1 +1,24 @@
-alert ('allo')
+class Student {
+    constructor(fullName, direction) {
+        this._fullName = fullName;
+        this._direction = direction;
+    }
+
+    showFullName(){
+        let result = this._fullName;
+        return result;
+    }
+    nameIncludes(data) {
+        if (this.showFullName().includes(data)) return console.log (true);
+        return console.log(false);
+    }
+    static studentBuilder(fullName, direction){
+        return new Student (fullName, direction);
+    }
+}
+    const stud1 = new Student ("Ivan Petrenko", "web");
+    const stud2 = new Student ("Sergiy Koval", "puthon");
+    const stud3 = Student.studentBuilder ("Ihor Kohut", "qc");
+
+    stud1.nameIncludes('Ivan'); 
+    stud1.nameIncludes('Denysenko');
